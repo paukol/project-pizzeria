972dc09be0c6a1c2df69619e946a65830761a7a8
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-03-28 21:00:44 +0200

    Add booking class 10.4

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,104 @@
 
-import {settings, select, templates, classNames} from './settings.js';
+import{settings, templates, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Bookings.js';
 
-const app= {
+const app = {
+
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    console.log('idFromHash', idFromHash);
+
+    let pageMachingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMachingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMachingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with this id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+
+    /* add class "active" to matching links, remove from non-matching */
+    for (let link  of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
 
   initData: function(){
     const thisApp = this;
-
     thisApp.data = {};
+
     const url = settings.db.url + '/' + settings.db.product;
 
     fetch(url)
-      .then(function (rawResponse) {
+      .then(function(rawResponse){
         return rawResponse.json();
       })
-      .then(function (parsedResponse) {
-        console.log('parsedPresponse', parsedResponse);
-        /* save parsedResponse as thisApp.data.products */
+      .then(function(parsedResponse){
+        console.log('parsedResponse:', parsedResponse);
+        /* save parsedResponse at thisApp.data.products */
         thisApp.data.products = parsedResponse;
 
         /* execute initMenu method */
-        app.initMenu();
+        thisApp.initMenu();
       });
 
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
+
   initMenu: function(){
     const thisApp = this;
     console.log('thisApp.data:', thisApp.data);
-
-    for(let productData in thisApp.data.products){
+    for (let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
+  },
+
+  initBooking: function(){
+    const thisApp = this;
+
+    const bookingWidgetContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingWidgetContainer);
   },
 
   init: function(){
@@ -44,21 +109,25 @@ const app= {
     console.log('settings:', settings);
     console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
+    thisApp.initBooking();
       
   },
 
-  initCart: function () {
+
+  initCart: function(){
     const thisApp = this;
 
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function(event){
+
+    this.productList.addEventListener('add-to-cart', function (event){
       app.cart.add(event.detail.product);
-    });  
-  }
+    });
+  },
 
 };
 
